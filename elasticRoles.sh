8779e29d79aa71b1f80ff7cff20e746d5a5759c7
mirage2012 <leo.mohit@gmail.com>
2019-04-25 10:51:22 +0530

    Update elasticRoles.sh

--- elasticRoles.sh
+++ elasticRoles.sh
@@ -1,2 +1,29 @@
-cat "This is elastic roles"
-# changehttp://127.0.0.1:8080/loginError
+#!/usr/bin/env bash
+curl -X POST -u ${DEVOPS_ELASTIC_USER}:${DEVOPS_ELASTIC_PASS} "${DEVOPS_ELASTIC}/_security/role/cmp.devops.user.upi" -H 'Content-Type: application/json' -d'
+{
+  "cluster" : [
+  ],
+  "indices" : [
+  ],
+  "applications" : [
+    {
+      "application" : "kibana-.kibana",
+      "privileges" : [
+        "read"
+      ],
+      "resources" : [
+        "*"
+      ]
+    },
+    {
+      "application" : "kibana-.kibana",
+      "privileges" : [
+        "space_all"
+      ],
+      "resources" : [
+        "space:upi"
+      ]
+    }
+  ]
+}
+'
