0b5371c8a98c9f6695fea4fc83bd173e8e87e637
mirage2012 <leo.mohit@gmail.com>
2019-04-25 17:56:37 +0530

    Update elasticRoles.sh

--- elasticRoles.sh
+++ elasticRoles.sh
@@ -45,7 +45,8 @@ for val in ${Teams[@]}; do
   ]
 }
 '
-printf -- '-%.0s' {1..100}; echo ""#Create Roll mappings for the above roles
+printf -- '-%.0s' {1..100}; echo ""
+#Create Roll mappings for the above roles
 
 curl -X PUT -u ${ELASTIC_USR}:${ELASTIC_PSW} "${DEVOPS_ELASTIC}:${ELASTIC_PORT}/_xpack/security/role_mapping/cmp.devops.user.$val" -H 'Content-Type: application/json' -d'
 {
