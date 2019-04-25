233f2b6a56d8b25ddac49deb0048bb0f29031fa0
mirage2012 <leo.mohit@gmail.com>
2019-04-25 12:27:35 +0530

    Update elasticRoles.sh

--- elasticRoles.sh
+++ elasticRoles.sh
@@ -41,3 +41,109 @@ curl -X POST -u ${ELASTIC_USR}:${ELASTIC_PSW} "${DEVOPS_ELASTIC}:${ELASTIC_PORT}
   ]
 }
 '
+curl -X POST -u ${ELASTIC_USR}:${ELASTIC_PSW} "${DEVOPS_ELASTIC}:${ELASTIC_PORT}/_security/role/" -H 'Content-Type: application/json' -d'
+{
+"cmp.devops.user.devops-data" : {
+    "cluster" : [ ],
+    "indices" : [
+      {
+        "names" : [
+          "edu*"
+        ],
+        "privileges" : [
+          "read",
+          "view_index_metadata"
+        ],
+        "field_security" : {
+          "grant" : [
+            "*"
+          ]
+        }
+      },
+      {
+        "names" : [
+          "stats*"
+        ],
+        "privileges" : [
+          "all"
+        ],
+        "field_security" : {
+          "grant" : [
+            "*"
+          ]
+        }
+      }
+    ],
+    "applications" : [
+      {
+        "application" : "kibana-.kibana",
+        "privileges" : [
+          "read"
+        ],
+        "resources" : [
+          "*"
+        ]
+      },
+      {
+        "application" : "kibana-.kibana",
+        "privileges" : [
+          "space_all"
+        ],
+        "resources" : [
+          "space:devops-data"
+        ]
+      }
+    ],
+    "run_as" : [ ],
+    "metadata" : { },
+    "transient_metadata" : {
+      "enabled" : true
+    }
+  },
+  "loris_ingest" : {
+    "cluster" : [
+      "monitor"
+    ],
+    "indices" : [
+      {
+        "names" : [
+          "loris*"
+        ],
+        "privileges" : [
+          "all"
+        ],
+        "field_security" : {
+          "grant" : [
+            "*"
+          ]
+        }
+      }
+    ],
+    "applications" : [
+      {
+        "application" : "kibana-.kibana",
+        "privileges" : [
+          "read"
+        ],
+        "resources" : [
+          "*"
+        ]
+      },
+      {
+        "application" : "kibana-.kibana",
+        "privileges" : [
+          "space_all"
+        ],
+        "resources" : [
+          "space:nexxis"
+        ]
+      }
+    ],
+    "run_as" : [ ],
+    "metadata" : { },
+    "transient_metadata" : {
+      "enabled" : true
+    }
+  }
+  }
+  '
