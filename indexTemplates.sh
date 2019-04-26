ba640afc67c232f6f6c5f248b1b39bf6ad74a7e8
mirage2012 <leo.mohit@gmail.com>
2019-04-26 13:34:38 +0530

    Update indexTemplates.sh

--- indexTemplates.sh
+++ indexTemplates.sh
@@ -1,3 +1,61 @@
-#!/bin/bash
-/bin/echo "This is Index Template file"
-# creating a change to generate changelog
+#!/usr/bin/env bash
+set +e
+
+echo curl -sX PUT -u ${ELASTIC_USR}:${ELASTIC_PSW} "${DEVOPS_ELASTIC}:${ELASTIC_PORT}/_template/template_1" -H 'Content-Type: application/json' -d '
+{
+  	"index_patterns": ["*.kube*"],
+
+  	"mappings": {
+  		"fluentd": {
+  			"properties": {
+  				"kubernetes": {
+  					"properties": {
+  						"container_name": {
+  							"type": "keyword"
+  						},
+                        "host" : {
+                            "type": "keyword"
+                        },
+                        "labels" : {
+                            "type": "keyword"
+                        },
+                        "env" : {
+                            "type": "keyword"
+                        },
+                        "envtype" : {
+                            "type": "keyword"
+                        },
+                        "pod-template-hash" : {
+                            "type": "keyword"
+                        },
+                        "release" : {
+                            "type": "keyword"
+                        },
+                        "master_url" : {
+                            "type": "keyword"
+                        },
+                        "namespace_id" : {
+                            "type": "keyword"
+                        },
+                        "namespace_labels" : {
+                            "type": "keyword"
+                        },
+                        "namespace_name" : {
+                            "type": "keyword"
+                        },
+                        "pod_id" : {
+                            "type": "keyword"
+                        },
+                        "pod_name" : {
+                            "type": "keyword"
+                        }
+
+                                
+  					}
+  				}
+  			}
+  		}
+  	}
+  }
+'
+
